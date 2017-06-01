ed935f0456cbf5f20cfee4c87831afcb471e4a87
Ville Rantala <ville.rantala@microsoft.com>
2017-06-01 17:06:51 +0300

    Index address so that filter can be used

--- contracts/Telemetry.sol
+++ contracts/Telemetry.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.4;
 contract Telemetry {
   mapping(address => string) latestTelemetry;
 
-  event TelemetryReceived(address from, string telemetry);
+  event TelemetryReceived(address indexed from, string telemetry);
 
   function sendTelemetry(string telemetry) {
     latestTelemetry[msg.sender] = telemetry;
